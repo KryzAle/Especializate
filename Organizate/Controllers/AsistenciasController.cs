63a3996594f38aa1efc55ab2ef56b6e2bee44322
Unknown <kryzale@gmail.com>
2019-01-17 17:46:15 -0500

    Creacion Cita

--- Organizate/Controllers/AsistenciasController.cs
+++ Organizate/Controllers/AsistenciasController.cs
@@ -12,6 +12,7 @@ namespace Organizate.Controllers
 {
     public class AsistenciasController : Controller
     {
+        public static Tema temaCita = new Tema();
         private DB_A44489_asistenciaEntities db = new DB_A44489_asistenciaEntities();
 
         // GET: Asistencias
@@ -19,11 +20,120 @@ namespace Organizate.Controllers
         {
             if (Request.IsAuthenticated)
             {
-                ViewBag.ins_est_id = new SelectList(db.Estudiante, "est_id", "est_nombre");
-                return View();
+                var asistencia = db.Asistencia.Include(a => a.Estudiante).Include(a => a.Tema);
+                return View(asistencia.ToList());
             }
             return RedirectToAction("Login", "Account", new { returnUrl = "~/Asistencia/Cita" });
 
+        }
+        public JsonResult TraerTemas(int id)
+        {
+            IList<Profesor_Materia> profesor_materia = db.Profesor_Materia.Where(t => t.pro_mat_mat_id == id ).ToList();
+            var listaTemasMat = new List<Tema>();
+            foreach (var itemProfesorMateria in profesor_materia)
+            {
+                foreach (var item in itemProfesorMateria.Tema)
+                {
+                    
+                        listaTemasMat.Add(item);
+                }
+            }
+           var data = new SelectList(listaTemasMat.ToList(), "tema_id", "tema_nombre");
+            return Json(data, JsonRequestBehavior.AllowGet);
+            
+        }
+        public ActionResult CreateCita()
+        {
+            if (Request.IsAuthenticated)
+            {
+                ViewBag.pro_mat_mat_id = new SelectList(db.Materia, "mat_id", "mat_nombre");
+                return View();
+            }
+            return RedirectToAction("Login", "Account", new { returnUrl = "~/Asistencia/CreateCita" });
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateCita(int tema_id)
+        {
+            temaCita= db.Tema.Find(tema_id);
+            
+
+            return RedirectToAction("RegistrarCita");
+        }
+        public ActionResult RegistrarCita()
+        {
+            Profesor_Materia profesor_materia = db.Profesor_Materia.Find(temaCita.tema_pro_mat_id);
+            List<Horario_Profesor> horario_profesor = db.Horario_Profesor.Where(x => x.hor_pro_pro_id == profesor_materia.pro_mat_pro_id).ToList();
+            ViewBag.asi_est_id = new SelectList(db.Estudiante, "est_id", "est_nombre");
+            return View(horario_profesor.ToList());
+        }
+        public ActionResult ConfirmarCita(int id)
+        {
+            Horario_Profesor horario = db.Horario_Profesor.Find(id);
+            Asistencia asistencia = new Asistencia();
+            asistencia.asi_tema_id = temaCita.tema_id;
+            asistencia.asi_hora_inicio = horario.hor_pro_hora_inicio;
+            DateTime dt = DateTime.Now;
+            var desicion = true;
+            do
+            {
+                int dia =(int) dt.DayOfWeek;
+                string dialetra = obtenerDia(dia);
+                if (dialetra.Equals(horario.hor_pro_dia))
+                {
+                    asistencia.asi_fecha = dt;
+                    desicion = false;
+                }
+                dt = dt.AddDays(1);
+            } while (desicion);
+            ViewBag.asi_est_id = new SelectList(db.Estudiante, "est_id", "est_nombre");
+            return View(asistencia);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ConfirmarCita(Asistencia asistencia)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Asistencia.Add(asistencia);
+                db.SaveChanges();
+                return RedirectToAction("Cita");
+            }
+
+            ViewBag.asi_est_id = new SelectList(db.Estudiante, "est_id", "est_nombre", asistencia.asi_est_id);
+            return View(asistencia);
+        }
+        public string obtenerDia(int dia)
+        {
+            string dialetra = "";
+            switch (dia)
+            {
+                
+                case 1:
+                    dialetra = "Lunes";
+                    break;
+                case 2:
+                    dialetra = "Martes";
+                    break;
+                case 3:
+                    dialetra = "Miercoles";
+                    break;
+                case 4:
+                    dialetra = "Jueves";
+                    break;
+                case 5:
+                    dialetra = "Viernes";
+                    break;
+                case 6:
+                    dialetra = "Sabado";
+                    break;
+                case 7:
+                    dialetra = "Domingo";
+                    break;
+
+            }
+            return dialetra;
         }
         public ActionResult Index()
         {
